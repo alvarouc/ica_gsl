fc9fddd56da90d9d197e6ad32cb5e307440f8673
Alvaro <alvarouc@gmail.com>
2015-06-29 11:31:54 -0600

    added function to extract column mean from matrix

--- util/util.c
+++ util/util.c
@@ -1,5 +1,20 @@
 #include "util.h"
 
+gsl_vector *matrix_mean(gsl_matrix *input){
+  size_t col;
+  size_t NCOL = input->size2;
+  gsl_vector_view a_col;
+  gsl_vector *mean = gsl_vector_alloc(NCOL) ;
+  for (col = 0; col < NCOL; col++) {
+    a_col = gsl_matrix_column(input, col);
+    gsl_vector_set(mean, col, gsl_stats_mean(a_col.vector.data,
+    a_col.vector.stride, a_col.vector.size));
+  }
+
+  return mean;
+
+}
+
 void fill_matrix_random(gsl_matrix *input){
 // Fill a GSL matrix with random numbers
   size_t NROW = input->size1;
@@ -9,7 +24,7 @@ void fill_matrix_random(gsl_matrix *input){
 
   for (i = 0; i < NROW; i++)
     for (j = 0; j < NCOL; j++){
-                  gsl_matrix_set(input, i, j, (double)(rand()));
+                  gsl_matrix_set(input, i, j, (double)(rand()%100));
           }
 
 }
@@ -20,8 +35,8 @@ void print_matrix_corner(gsl_matrix *input){
 
   printf ("\n\nMatrix size: %zux%zu\n", input->size1, input->size2);
 
-  size_t NROW = input->size1 < 3 ? input->size1 : 3;
-  size_t NCOL = input->size2 < 3 ? input->size2 : 3;
+  size_t NROW = input->size1 < 6 ? input->size1 : 6;
+  size_t NCOL = input->size2 < 6 ? input->size2 : 6;
 
   for (i = 0; i < NROW; i++){
     for (j = 0;j < NCOL; j++) {
--- util/util.h
+++ util/util.h
@@ -3,6 +3,11 @@
 #define UTILS_H_
 // #include <lapacke.h>
 #include <gsl/gsl_matrix.h>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_blas.h>
+#include <gsl/gsl_statistics.h>
+
+gsl_vector *matrix_mean(gsl_matrix *input);
 void fill_matrix_random(gsl_matrix *input);
 void print_matrix_corner(gsl_matrix *input);
 
